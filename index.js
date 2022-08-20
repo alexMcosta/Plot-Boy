c8916ac8fdd151c8d0cb9c65ca15b59a5d266602
Alex Costa <alexcosta@Alexs-MacBook-Pro.local>
2022-08-20 02:37:31 -0400

    fixed issue

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-require("dotenv").config()
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
-//const { token } = require('./config.json');
-
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -15,16 +22,16 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'moms') {
-		await interaction.reply('Hollow, Darto, and Autumn');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-// Login to Discord with your client's token
-client.login(process.env.BOT_TOKEN)
+client.login(token);
